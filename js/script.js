05752999c0ce11487976ab5614adbc4dc70dd079
Maciej <maciej.zelazo@protonmail.com>
2020-07-28 21:12:28 +0200

    list made

--- js/script.js
+++ js/script.js
@@ -23,4 +23,67 @@ $(".owl-categories").owlCarousel({
     }
 });
 
-$('.about .img-fluid').css('transform', 'translateY(-100%)');
+
+$(window).scroll(function () {
+    const elem = ('.about');
+    let docViewTop = $(window).scrollTop();
+    let docViewBottom = docViewTop + $(window).height();
+
+    let elemTop = $(elem).offset().top;
+    let elemBottom = elemTop + $(elem).height();
+
+    if ((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+        $('.about .img-fluid').css('transform', 'translateY(-100%)');
+    }
+});
+
+function headerChangePlace() {
+    if ((window.innerWidth) < 380) {
+        $('.header--subpages .header__h1').prependTo('.navbar-collapse .row');
+    } else {
+        $('.header--subpages .header__h1').appendTo('.navbar');
+        $('.header--subpages .navbar-toggler').appendTo('.navbar');
+
+    }
+    if ((window.innerWidth) <= 992) {
+        $('.header--subpages .navbar-collapse').appendTo('.navbar');
+    } else {
+        $('.header--subpages .navbar-collapse').prependTo('.navbar');
+        $('.header--subpages .navbar-brand').prependTo('.navbar');
+    }
+}
+
+
+$(document).ready(function () {
+    window.onresize = function () {
+        headerChangePlace();
+    }
+
+    headerChangePlace();
+    if (window.innerWidth <= 992) {
+        headerOnScroll();
+    }
+});
+
+
+
+
+function headerOnScroll() {
+    let prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function () {
+        let currentScrollPos = window.pageYOffset;
+
+        if (this.window.pageYOffset > 105) {
+
+
+            if (prevScrollpos > currentScrollPos) {
+                document.getElementById("headerSubpages").style.top = "0";
+            } else {
+                document.getElementById("headerSubpages").style.top = "-145px";
+            }
+
+            prevScrollpos = currentScrollPos;
+        }
+    }
+}
