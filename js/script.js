fcd4f66895d256f4574727e02935f487edae674f
Maciej <maciej.zelazo@protonmail.com>
2020-07-28 16:00:39 +0200

    animation

--- js/script.js
+++ js/script.js
@@ -6,17 +6,21 @@ $(".owl-categories").owlCarousel({
     nav: false,
     dots: false,
     lazyLoad: true,
-    autoplay: false,
+    autoplay: true,
     margin: 16,
     responsive: {
         0: {
-            items: 1
+            items: 1,
+            autoplayTimeout: 1500
         },
         700: {
-            items: 3
+            items: 3,
+            autoplayTimeout: 4000
         },
         1200: {
             items: 5
         }
     }
 });
+
+$('.img-fluid').css('transform', 'translateY(-100%)');
